e6edf890d18bab9619709af574e698f0b8fc7735
Amirkhan <aak82183@gmail.com>
2023-01-29 17:35:28 +0300

    Create CharacterFooterView

---
+++ RickAndMorty/Screens/CharactersView/CharacterFooterView.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterFooterView.swift
+//  RickAndMorty
+//
+//  Created by Amirkhan Akaev on 29.01.2023.
+//
+
+import UIKit
+
+final class CharacterFooterView: UICollectionReusableView {
+    let activity = UIActivityIndicatorView(style: .medium)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        activity.startAnimating()
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(activity)
+        
+        NSLayoutConstraint.activate([
+            activity.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activity.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+}
